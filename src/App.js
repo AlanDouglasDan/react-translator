befc6bbd07fcb3ff046d6db9b810e63662640dd1
AlanDouglasDan <aland6209@gmail.com>
2022-03-12 23:34:27 +0100

    api integration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  // const [text, setText] = useState('');
+
+  const handleSubmit = async ()=> {
+    const textarea = document.getElementById('textAreaExample1').value;
+    const fromSelector = document.getElementById('fromSelector').value;
+    const toSelector = document.getElementById('toSelector').value;
+    const body = {
+      'text': textarea,
+      'from': fromSelector,
+      'to': toSelector
+    }
+    const response = await axios.post('https://alan-translator-api.herokuapp.com/translate', body);
+    const value = response.data.text;
+    document.getElementById('textAreaExample2').innerHTML = value;
+    console.log(response);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,8 +31,8 @@ function App() {
           <div className='col-xs-6'>
             <div className="form-outline">
               <textarea className="form-control special_textarea" id="textAreaExample1" rows="4" placeholder='Translate from'></textarea>
-              <select className="form-select" aria-label="Default select example">
-                <option selected>Select language</option>
+              <select className="form-select" aria-label="Default select example" id="fromSelector">
+                <option>Select language</option>
                 <option value="en">English</option>
                 <option value="fr">French</option>
                 <option value="de">German</option>
@@ -27,8 +47,8 @@ function App() {
           <div className='col-xs-6'>
             <div className="form-outline">
               <textarea className="form-control special_textarea" id="textAreaExample2" rows="4" placeholder='Translate to'></textarea>
-              <select className="form-select" aria-label="Default select example">
-                <option selected>Select language</option>
+              <select className="form-select" aria-label="Default select example" id="toSelector">
+                <option>Select language</option>
                 <option value="en">English</option>
                 <option value="fr">French</option>
                 <option value="de">German</option>
@@ -41,7 +61,7 @@ function App() {
           </div>
         </div>  
         <div className='btn-container container'>
-          <button type="button" class="btn btn-info">Translate</button>
+          <button onClick={handleSubmit}type="button" className="btn btn-info">Translate</button>
         </div>
       </header>
     </div>
